d35f360e4c8be81b8d52166d5d6a0b935f3ea898
Michael <michael@gomerits.com>
2024-12-15 21:53:18 +0000

    feat: manage db migrations separately

--- scripts/deploy_gcp.sh
+++ scripts/deploy_gcp.sh
@@ -1,63 +1,81 @@
-# scripts/deploy_gcp.sh
 #!/bin/bash
 set -euo pipefail
 
 # Configuration
 NAMESPACE="todo-app"
 APP_HELM_CHART_PATH="./helm/todo-app"
+MIGRATIONS_HELM_CHART_PATH="./helm/migrations"
 DB_HELM_CHART_PATH="./helm/postgres"
-export REPOSITORY_ID="sre-challenge-repository"
-export PROJECT_ID="sre-challenge-b71f132d"
-export REGION="europe-west1"
-export APP_IMAGE_NAME="todo-app:latest"
-export MIGRATIONS_IMAGE_NAME="todo-app-migrations:latest"
+TAG="1.0.2"
+APP_IMAGE_NAME="todo-app:${TAG}"
+MIGRATIONS_IMAGE_NAME="todo-app-migrations:${TAG}"
+PROJECT_ID="sre-challenge-b71f132d"
+REGION="europe-west1"
+IMAGE_REPOSITORY="${REGION}-docker.pkg.dev/${PROJECT_ID}/sre-challenge-repository"
 
 # Staging specific config
 CLUSTER_NAME="sre-challenge-cluster-s"
-export IP_ADDRESS="34.54.49.46"
-export IP_NAME="sre-challenge-ip-s"
+IP_ADDRESS="34.54.49.46"
+IP_NAME="sre-challenge-ip-s"
+
+export TAG
+export IMAGE_REPOSITORY
+export IP_ADDRESS
+export IP_NAME
 
 # Get GKE credentials
 echo "Getting GKE cluster credentials..."
 gcloud container clusters get-credentials ${CLUSTER_NAME} --region ${REGION} --project ${PROJECT_ID}
-gcloud auth configure-docker ${REGION}-docker.pkg.dev
 
 # Create namespace if it doesn't exist
 kubectl create namespace ${NAMESPACE} --dry-run=client -o yaml | kubectl apply -f -
 
+# Set current context to namespace
+kubectl config set-context $(kubectl config current-context) --namespace ${NAMESPACE}
+
 # # Build and push Docker image
 # echo "Building and pushing Docker images..."
+# gcloud auth configure-docker ${REGION}-docker.pkg.dev
 # # docker build -t ${APP_IMAGE_NAME} -f src/Dockerfile .
-# docker tag ${APP_IMAGE_NAME} ${REGION}-docker.pkg.dev/${PROJECT_ID}/${REPOSITORY_ID}/${APP_IMAGE_NAME}
-# docker push ${REGION}-docker.pkg.dev/${PROJECT_ID}/${REPOSITORY_ID}/${APP_IMAGE_NAME}
+# docker tag "${APP_IMAGE_NAME}" "${IMAGE_REPOSITORY}/${APP_IMAGE_NAME}"
+# docker push "${IMAGE_REPOSITORY}/${APP_IMAGE_NAME}"
 
 # # docker build -t ${MIGRATIONS_IMAGE_NAME} -f ./migrations/Dockerfile . .
-# docker tag ${MIGRATIONS_IMAGE_NAME} ${REGION}-docker.pkg.dev/${PROJECT_ID}/${REPOSITORY_ID}/${MIGRATIONS_IMAGE_NAME}
-# docker push ${REGION}-docker.pkg.dev/${PROJECT_ID}/${REPOSITORY_ID}/${MIGRATIONS_IMAGE_NAME}
+# docker tag "${MIGRATIONS_IMAGE_NAME}" "${IMAGE_REPOSITORY}/${MIGRATIONS_IMAGE_NAME}"
+# docker push "${IMAGE_REPOSITORY}/${MIGRATIONS_IMAGE_NAME}"
 
 # Install/upgrade PostgreSQL chart
-# echo "Deploying PostgreSQL..."
-# helm upgrade --install postgres ${DB_HELM_CHART_PATH} \
-#   --namespace ${NAMESPACE} \
-#   --values ${DB_HELM_CHART_PATH}/values.yaml \
-#   --wait \
-#   --debug
+echo "Deploying PostgreSQL..."
+helm upgrade --install postgres ${DB_HELM_CHART_PATH} \
+  --namespace ${NAMESPACE} \
+  --values ${DB_HELM_CHART_PATH}/values.yaml \
+  --wait \
+  --debug
+
+# Install/upgrade app db migrations chart
+echo "Deploying migrations..."
+helm upgrade --install migrations ${MIGRATIONS_HELM_CHART_PATH} \
+  --namespace ${NAMESPACE} \
+  --values ${MIGRATIONS_HELM_CHART_PATH}/values.yaml \
+  --set image.repository="${IMAGE_REPOSITORY}/todo-app-migrations" \
+  --set image.tag="${TAG}" \
+  --set image.pullPolicy="IfNotPresent" \
+  --wait \
+  --debug
 
 # Create temporary values file from template
-envsubst < ${APP_HELM_CHART_PATH}/values-gcp.yaml > /tmp/values-gcp.yaml
+envsubst < "${APP_HELM_CHART_PATH}/values-gcp.yaml" > /tmp/values-gcp.yaml
 
 # Install/upgrade Todo app chart
 echo "Deploying Todo app..."
 helm upgrade --install todo-app ${APP_HELM_CHART_PATH} \
   --namespace ${NAMESPACE} \
-  --values ${APP_HELM_CHART_PATH}/values.yaml \
+  --values "${APP_HELM_CHART_PATH}/values.yaml" \
   --values /tmp/values-gcp.yaml \
-  --set image.repository="${REGION}-docker.pkg.dev/${PROJECT_ID}/${REPOSITORY_ID}/todo-app" \
-  --set image.tag="latest" \
   --wait \
   --debug
 
 # Clean up
-# rm /tmp/values-gcp.yaml
+rm /tmp/values-gcp.yaml
 
 echo "Deployment completed!"
--- scripts/deploy_k3d.sh
+++ scripts/deploy_k3d.sh
@@ -1,4 +1,3 @@
-# scripts/deploy_k3d.sh
 #!/bin/bash
 
 set -euo pipefail
@@ -8,10 +7,10 @@ CLUSTER_NAME="todo-app-cluster"
 NAMESPACE="todo-app"
 SERVICE_NAME="todo-app"
 APP_HELM_CHART_PATH="./helm/todo-app"
+MIGRATIONS_HELM_CHART_PATH="./helm/migrations"
 DB_HELM_CHART_PATH="./helm/postgres"
 APP_IMAGE_NAME="todo-app:latest"
 MIGRATIONS_IMAGE_NAME="todo-app-migrations:latest"
-VALUES_FILE="./values.yaml"
 
 # Create k3d cluster
 if k3d cluster list | grep -q "^${CLUSTER_NAME} "; then
@@ -23,29 +22,17 @@ fi
 
 # Build and push migrations Docker image
 echo "Building Docker image: $MIGRATIONS_IMAGE_NAME..."
-docker build -t $MIGRATIONS_IMAGE_NAME -f ./migrations/Dockerfile . || {
-  echo "Failed to build migrations Docker image. Exiting."
-  exit 1
-}
+docker build -t $MIGRATIONS_IMAGE_NAME -f ./migrations/Dockerfile .
 
 echo "Pushing Docker image to cluster..."
-k3d image import $MIGRATIONS_IMAGE_NAME -c $CLUSTER_NAME || {
-  echo "Failed to push migrations Docker image to registry. Exiting."
-  exit 1
-}
+k3d image import $MIGRATIONS_IMAGE_NAME -c $CLUSTER_NAME
 
 # Build and push app Docker image
 echo "Building Docker image: $APP_IMAGE_NAME..."
-docker build -t $APP_IMAGE_NAME -f ./src/Dockerfile . || {
-  echo "Failed to build app Docker image. Exiting."
-  exit 1
-}
+docker build -t $APP_IMAGE_NAME -f ./src/Dockerfile .
 
 echo "Pushing Docker image to cluster..."
-k3d image import $APP_IMAGE_NAME -c $CLUSTER_NAME || {
-  echo "Failed to push app Docker image to registry. Exiting."
-  exit 1
-}
+k3d image import $APP_IMAGE_NAME -c $CLUSTER_NAME
 
 # Create namespace
 echo "Creating namespace: $NAMESPACE..."
@@ -53,23 +40,15 @@ kubectl create namespace $NAMESPACE || echo "Namespace $NAMESPACE already exists
 
 # Deploy db Helm chart
 echo "Deploying db Helm chart..."
-helm upgrade --install postgres $DB_HELM_CHART_PATH --namespace $NAMESPACE || {
-  echo "Failed to deploy db Helm chart. Exiting."
-  exit 1
-}
+helm upgrade --install postgres $DB_HELM_CHART_PATH --namespace $NAMESPACE --wait --debug
+
+# Deploy app db migrations Helm chart
+echo "Deploying migrations Helm chart..."
+helm upgrade --install migrations $MIGRATIONS_HELM_CHART_PATH --namespace $NAMESPACE --wait --debug
 
 # Deploy app Helm chart
 echo "Deploying app Helm chart..."
-helm upgrade --install todo-app $APP_HELM_CHART_PATH --namespace $NAMESPACE || {
-  echo "Failed to deploy app Helm chart. Exiting."
-  exit 1
-}
-
-# # Verify deployment
-# echo "Verifying deployment..."
-# kubectl get pods -n $NAMESPACE || echo "Error getting pods."
-# kubectl get svc -n $NAMESPACE || echo "Error getting services."
-# kubectl get ingress -n $NAMESPACE || echo "No ingress configured in $NAMESPACE namespace."
+helm upgrade --install todo-app $APP_HELM_CHART_PATH --namespace $NAMESPACE --wait --debug
 
 # Port forward for local testing (optional)
 echo "Port-forwarding the service for local access..."
@@ -77,11 +56,4 @@ kubectl port-forward svc/$SERVICE_NAME 8080:80 -n $NAMESPACE &
 PORT_FORWARD_PID=$!
 
 echo "Deployment completed. Access your app at http://localhost:8080"
-
-# Function to clean up port forwarding on exit
-function cleanup {
-  echo "Cleaning up port forwarding..."
-  kill $PORT_FORWARD_PID
-}
-
-trap cleanup EXIT
+echo "Port-forward PID: $PORT_FORWARD_PID"
