7b3176b499c4b2dca83128bea91792ab541a84ad
Michael <michael@gomerits.com>
2024-12-24 12:40:59 +0000

    feat: add cluster kms encryption

---
+++ terraform/modules/baseline/kms.tf
@@ -0,0 +1,19 @@
+resource "google_kms_key_ring" "gke" {
+  name     = "gke-keyring"
+  location = "global"
+  project  = local.project_id
+}
+
+resource "google_kms_crypto_key" "gke" {
+  name     = "gke-key"
+  key_ring = google_kms_key_ring.gke.id
+}
+
+resource "google_kms_crypto_key_iam_binding" "gke" {
+  crypto_key_id = google_kms_crypto_key.gke.id
+  role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
+  members = [
+    "serviceAccount:service-${data.terraform_remote_state.project.outputs.project_number}@container-engine-robot.iam.gserviceaccount.com",
+    "serviceAccount:${google_service_account.gke_sa.email}"
+  ]
+}
